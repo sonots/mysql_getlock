58c5b6c3564987f186fa5c71b666c0f00016f622
sonots <sonots@gmail.com>
2016-08-28 17:54:55 +0900

    trivial: log_head

--- lib/mysql_getlock.rb
+++ lib/mysql_getlock.rb
@@ -26,17 +26,17 @@ class MysqlGetlock
       raise Error, "get_lock() is already issued in the same connection for '#{current_session_key}'"
     end
 
-    logger.info { "MysqlGetlock: Wait acquiring a lock: #{key}" } if logger
+    logger.info { "#{log_head}Wait acquiring a mysql lock '#{key}'" } if logger
     results = mysql2.query(%Q[select get_lock('#{key}', #{timeout})], as: :array)
     case results.to_a.first.first
     when 1
-      logger.info { "MysqlGetlock: Acquired a lock: #{key}" } if logger
+      logger.info { "#{log_head}Acquired a mysql lock '#{key}'" } if logger
       set_current_session_key(key)
     when 0
-      logger.info { "MysqlGetlock: Timeout to acquire a lock: #{key}" } if logger
+      logger.info { "#{log_head}Timeout to acquire a mysql lock '#{key}'" } if logger
       release_current_session_key
     else # nil
-      logger.info { "MysqlGetlock: Unknown Error to acquire a lock: #{key}" } if logger
+      logger.info { "#{log_head}Unknown Error to acquire a mysql lock '#{key}'" } if logger
       release_current_session_key
     end
   end
@@ -50,13 +50,13 @@ class MysqlGetlock
     release_current_session_key
     case results.to_a.first.first
     when 1
-      logger.info { "MysqlGetlock: Released a lock: #{key}" } if logger
+      logger.info { "#{log_head}Released a mysql lock '#{key}'" } if logger
       true
     when 0
-      logger.info { "MysqlGetlock: Failed to release since somebody else locked: #{key}" } if logger
+      logger.info { "#{log_head}Failed to release a mysql lock since somebody else locked '#{key}'" } if logger
       false
     else # nil
-      logger.info { "MysqlGetlock: Lock did not exist: #{key}" } if logger
+      logger.info { "#{log_head}Mysql lock did not exist '#{key}'" } if logger
       true
     end
   end
@@ -76,6 +76,10 @@ class MysqlGetlock
   end
 
   private
+
+  def log_head
+    "PID-#{::Process.pid} TID-#{::Thread.current.object_id.to_s(36)}: "
+  end
 
   # From MySQL 5.7.5, multiple simultaneous locks can be acquired
   def multiple_lockable?
